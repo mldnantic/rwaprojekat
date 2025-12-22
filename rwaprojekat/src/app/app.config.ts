b1b268dddddfba9b0e4ed99fb3b09ac455c6e83b
mldnantic <m.antic@elfak.rs>
2025-12-22 13:49:41 +0100

    scss mockup added

--- rwaprojekat/src/app/app.config.ts
+++ rwaprojekat/src/app/app.config.ts
@@ -1,13 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { metaReducers, reducers } from './app.state';
 import { provideEffects } from '@ngrx/effects';
 import { PartsEffects } from './store/part.effect';
-import { metaReducers, reducers } from './app.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- rwaprojekat/src/app/components/list/list.component.ts
+++ rwaprojekat/src/app/components/list/list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 import { Observable, of } from 'rxjs';
+import { PartsService } from '../../services/parts.service';
+import { Store } from '@ngrx/store';
 import { Part } from '../../models/part';
 import { AppState } from '../../app.state';
-import { AsyncPipe } from '@angular/common';
 import { PartComponent } from "../part/part.component";
 import { loadParts } from '../../store/part.action';
 import { selectPartList } from '../../store/part.selector';
-import { PartsService } from '../../services/parts.service';
 
 @Component({
   selector: 'list',
