e39dc2c92dfd39c415b855ecc8f197bc4ddd408e
mldnantic <m.antic@elfak.rs>
2025-12-23 19:54:48 +0100

    rudimental routes ok, for loop in list html now tracks only id so no more warnings show when switching pages

--- rwaprojekat/src/app/app.component.ts
+++ rwaprojekat/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { ListComponent } from './components/list/list.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ListComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- rwaprojekat/src/app/app.routes.ts
+++ rwaprojekat/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { SignUpComponent } from './components/signup/signup.component';
+import { NotFoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: ListComponent
+    },
+    {
+        path:'signup',
+        component: SignUpComponent
+    },
+    {
+        path:'**',
+        component: NotFoundComponent
+    }
+];
