ef6900ac306fb02acf664183b417544417110727
mldnantic <m.antic@elfak.rs>
2025-12-24 10:43:02 +0100

    slightly improved look and feel, sign up page needs more polishing

--- rwaprojekat/src/app/app.component.ts
+++ rwaprojekat/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = "RWA projekat";
 }
