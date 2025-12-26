8f5be350129c971961ace4f9500738e73a6ea6bf
mldnantic <m.antic@elfak.rs>
2025-12-26 09:20:05 +0100

    nav bar working

--- rwaprojekat/src/app/app.component.ts
+++ rwaprojekat/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, MatButtonModule],
+  imports: [RouterOutlet, RouterLink, MatButtonModule, MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  links = ['','signup'];
+  activeLink = this.links[0];
+
+  
 }
