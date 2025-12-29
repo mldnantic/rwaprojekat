4170a1b9505d65694ebb5a5b8f2c87f831a1e669
mldnantic <m.antic@elfak.rs>
2025-12-29 12:07:03 +0100

    added placeholder search filter options

--- rwaprojekat/src/app/components/list/list.component.ts
+++ rwaprojekat/src/app/components/list/list.component.ts
@@ -9,10 +9,12 @@ import { PartComponent } from "../part/part.component";
 import { loadParts } from '../../store/part.action';
 import { selectPartList } from '../../store/part.selector';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'list',
-  imports: [AsyncPipe, PartComponent, MatGridListModule],
+  imports: [AsyncPipe, PartComponent, MatGridListModule, MatListModule, MatCardModule],
   providers:[PartsService, Store],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
@@ -22,6 +24,8 @@ export class ListComponent implements OnInit {
   part$: Observable<Part[]> = of([]);
 
   constructor(private store: Store<AppState>) { }
+
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 
   ngOnInit(): void {
     this.store.dispatch(loadParts());
